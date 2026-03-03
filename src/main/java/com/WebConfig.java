33dc18a9546cd4a28faae2295cf4623e7889e08a
Brehin Ethan <80754323+ebrehin@users.noreply.github.com>
2026-03-03 15:15:34 +0100

    Update nom conteneur + cors + port serveur

--- src/main/java/com/WebConfig.java
+++ src/main/java/com/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization");
     }
